ca243da922bf6163cc98ad744a853a83847dace8
Granger <GrangerAbuhoff@gmail.com>
2014-05-03 21:19:03 -0400

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,34 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Investor.destroy_all
+Investment.destroy_all
+
+david = HTTParty.get("http://api.crunchbase.com/v/2/person/david-tisch?user_key=f37ccc5c5ba5deb17395cecee16778cd")
+people = HTTParty.get("http://api.crunchbase.com/v/2/people?user_key=f37ccc5c5ba5deb17395cecee16778cd")
+@people = people["data"]["items"]
+
+# people_one = HTTParty.get("http://api.crunchbase.com/v/2/people?user_key=f37ccc5c5ba5deb17395cecee16778cd&page=2")
+# @people_one = people_one["data"]["items"]
+#binding.pry
+all_paths = @people.map {|person| person["path"]}
+
+investors = {}
+
+50.times do |ind|
+  person = @people[ind]
+  path = person["path"]
+  person_response = HTTParty.get("http://api.crunchbase.com/v/2/"+ path +"?user_key=f37ccc5c5ba5deb17395cecee16778cd")
+
+ 
+  if person_response["data"]["relationships"]["investments"]
+    investor = Investor.create {name: person["name"], path: person["path"]}
+    
+    person_response["data"]["relationships"]["investments"].first["items"].each do |investment|
+      fund_path = investment["funding_round"]["path"]
+      invest_response = HTTParty.get("http://api.crunchbase.com/v/2/"+ fund_path +"?user_key=f37ccc5c5ba5deb17395cecee16778cd")
+      type = invest_response["data"]["properties"]["funding_type"]
+      date = invest_response["data"]["properties"]["announced_on"]
+      Investment.create {investor_id: investor.id, round: type, date: date}
+    end
+    #investors[person] = [person_response["data"]["relationships"]["investments"]] 
+  end
+
+end
